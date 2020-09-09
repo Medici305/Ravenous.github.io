efa946b4da83498621658821370d2e97c513b0b9
unknown <JulienOsman25@hotmail.com>
2020-09-09 20:06:51 +0100

    Updates

---
+++ precache-manifest.7622e25439b5d03bc978ce54644f3aef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8edbdcde6797ec615092ca589ff427",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a653f080b4d9490f4fc8",
+    "url": "/static/css/main.0c4c55ab.chunk.css"
+  },
+  {
+    "revision": "9925d90d1471d1ce66f7",
+    "url": "/static/js/2.99d41395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99d41395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a653f080b4d9490f4fc8",
+    "url": "/static/js/main.2ce6aa45.chunk.js"
+  },
+  {
+    "revision": "0b7eb9d469061bce3505",
+    "url": "/static/js/runtime-main.140093ed.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Ravenous.github.io/precache-manifest.4b9de3bd8049a95762fd203b17d3706d.js"
+  "/precache-manifest.7622e25439b5d03bc978ce54644f3aef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ravenous.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
